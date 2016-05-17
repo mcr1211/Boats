ef61f100c09f2913b786f0cef9c8371b8c498b11
Elio <Elio@LAPTOP-R2J5CDRF>
2016-05-17 16:51:09 +0200

    Merge origin/master
    Conflicts:
    	PracticaClasse/src/practicaclasse/BoatInc.java

--- src/com/Boat/Empresa/Empresa.java
+++ src/com/Boat/Empresa/Empresa.java
@@ -5,6 +5,11 @@
  */
 package com.Boat.Empresa;
 
+import com.Boat.Operacions.Lloguer;
+import com.Boat.Operacions.Reparacions;
+import com.Boat.Operacions.Venda;
+import com.Boat.Persona.Client;
+import com.Boat.Persona.Empleat;
 import com.Boat.Vaixell.Vaixell;
 import java.util.ArrayList;
 
@@ -16,4 +21,85 @@ public class Empresa {
     
     private String nom;
     private ArrayList <Vaixell> llistaVaixell;
+    private ArrayList <Client> llistaClient;
+    private ArrayList <Empleat> llistaEmpleat;
+    private ArrayList <Venda> llistaVenda;
+    private ArrayList <Lloguer> llistaLloguer;
+    private ArrayList <Reparacions> llistaReparacions;
+
+    public Empresa(String nom) {
+        this.nom = nom;
+        this.llistaVaixell = llistaVaixell;
+        this.llistaClient = llistaClient;
+        this.llistaEmpleat = llistaEmpleat;
+        this.llistaVenda = llistaVenda;
+        this.llistaLloguer = llistaLloguer;
+        this.llistaReparacions = llistaReparacions;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ArrayList<Vaixell> getLlistaVaixell() {
+        return llistaVaixell;
+    }
+
+    public void setLlistaVaixell(ArrayList<Vaixell> llistaVaixell) {
+        this.llistaVaixell = llistaVaixell;
+    }
+
+    public ArrayList<Client> getLlistaClient() {
+        return llistaClient;
+    }
+
+    public void setLlistaClient(ArrayList<Client> llistaClient) {
+        this.llistaClient = llistaClient;
+    }
+
+    public ArrayList<Empleat> getLlistaEmpleat() {
+        return llistaEmpleat;
+    }
+
+    public void setLlistaEmpleat(ArrayList<Empleat> llistaEmpleat) {
+        this.llistaEmpleat = llistaEmpleat;
+    }
+
+    public ArrayList<Venda> getLlistaVenda() {
+        return llistaVenda;
+    }
+
+    public void setLlistaVenda(ArrayList<Venda> llistaVenda) {
+        this.llistaVenda = llistaVenda;
+    }
+
+    public ArrayList<Lloguer> getLlistaLloguer() {
+        return llistaLloguer;
+    }
+
+    public void setLlistaLloguer(ArrayList<Lloguer> llistaLloguer) {
+        this.llistaLloguer = llistaLloguer;
+    }
+
+    public ArrayList<Reparacions> getLlistaReparacions() {
+        return llistaReparacions;
+    }
+
+    public void setLlistaReparacions(ArrayList<Reparacions> llistaReparacions) {
+        this.llistaReparacions = llistaReparacions;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Empresa{" + "nom=" + nom + ", llistaVaixell=" + llistaVaixell + ", llistaClient=" + llistaClient + ", llistaEmpleat=" + llistaEmpleat + ", llistaVenda=" + llistaVenda + ", llistaLloguer=" + llistaLloguer + ", llistaReparacions=" + llistaReparacions + '}';
+    }
+    
+    
+    
 }
