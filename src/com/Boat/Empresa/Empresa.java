d6cab009132217b0118726cc917c9d8a20c21133
Elio <Elio@LAPTOP-R2J5CDRF>
2016-05-17 16:43:54 +0200

    Merge origin/master
    Conflicts:
    	PracticaClasse/src/practicaclasse/BoatInc.java

---
+++ src/com/Boat/Empresa/Empresa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Boat.Empresa;
+
+import com.Boat.Vaixell.Vaixell;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Elio
+ */
+public class Empresa {
+    
+    private String nom;
+    private ArrayList <Vaixell> llistaVaixell;
+}
