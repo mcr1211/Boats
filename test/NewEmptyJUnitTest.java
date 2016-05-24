9b57aa6fe00d4b723da3a42bd20eef18b69e8e4c
Elio <Elio@LAPTOP-R2J5CDRF>
2016-05-24 11:01:07 +0200

    Merge origin/master
    Conflicts:
    	src/com/Boat/Empresa/Empresa.java
    	src/com/Boat/Persona/Empleat.java

---
+++ test/NewEmptyJUnitTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Elio
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+}
